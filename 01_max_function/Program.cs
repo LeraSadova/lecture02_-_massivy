2c02cbf7e5f5ac30eb42dd120e408248e4a4d5ac
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-08 12:47:06 +0300

    третий вариант решения

--- 01_max_function/Program.cs
+++ 01_max_function/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 int Max(int arg1, int arg2, int arg3)
 {
-    int result=arg2;
+    int result=arg1;
     if (arg2>result) result=arg2;
     if (arg3>result) result=arg3;
     return result;

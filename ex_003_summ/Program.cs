f47f1c6a554730245c0a3dbd4368486fc874e36b
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-09 09:10:13 +0300

    перенесла папки из первой лекции сюда

---
+++ ex_003_summ/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA=new Random().Next(1,10); //случайное число из 0 1 2 3 и т.д. до 9
+Console.WriteLine(numberA);
+int numberB=new Random().Next(1,10); //случайное число из 0 1 2 3 и т.д. до 9
+Console.WriteLine(numberB);
+int result=numberA+numberB;
+Console.WriteLine(result);
+
---
+++ ex_005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username=Console.ReadLine(); // считываем имя пользователя
+
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, это же Машенька!!! :) ");
+}
+else
+{
+Console.Write("Приветик, ");
+Console.WriteLine(username);
+}
